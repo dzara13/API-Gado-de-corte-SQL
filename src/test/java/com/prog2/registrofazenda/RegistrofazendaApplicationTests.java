6c0173b0e949a058d579e0d3ede3e3498fea500c
Daniel Zara <danielzara2151@gmail.com>
2022-06-08 08:57:59 -0300

    Projeto importado do Spring Initializr

---
+++ src/test/java/com/prog2/registrofazenda/RegistrofazendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prog2.registrofazenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrofazendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
