39ce137040a040fb5471e092d7663bc8eb8a228e
Daniel Zara <danielzara2151@gmail.com>
2022-06-14 17:16:11 -0300

    sql e tipos de dados modificados

--- src/main/resources/db/migration/V002__migration.sql
+++ src/main/resources/db/migration/V002__migration.sql
@@ -1,18 +1,7 @@
-create table mae
-(
-    id bigint not null auto_increment
-        primary key
-);
-
 create table animal
 (
-    id         bigint auto_increment
-        primary key,
+    id         bigint auto_increment primary key,
     numero     int  not null,
     sexo       char not null,
-    nascimento date not null,
-    constraint animal_numero_uindex
-        unique (numero),
-    constraint animal_mae_id_fk
-        foreign key (id) references mae (id)
+    nascimento date not null
 );
