4f4e3c8cc082dd076c3739be1631cdb3621bcdb0
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-07-07 09:17:58 -0300

    feat: busca dos animais na API e exibição básica

--- src/frontend/src/pages/Home.tsx
+++ src/frontend/src/pages/Home.tsx
@@ -1,5 +1,61 @@
+import { Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react"
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+interface Animal {
+    id: number,
+    numero: number,
+    sexo: string,
+    nascimento: string,
+    marca: string,
+    desmama: boolean
+}
+
+const baseURL = "http://localhost:8080/animais"
+
 export function Home() {
-    return <h1>Página principal</h1>
+    const [animals, setAnimals] = useState([])
+
+    function getAnimalsFromApi() {
+        axios.get(baseURL).then(response => {
+            setAnimals(response.data)
+        })
+    }
+
+    useEffect(() => {
+        getAnimalsFromApi()
+    }, [])
+
+    return (
+        <TableContainer>
+            <Table>
+                <Thead>
+                    <Tr>
+                        <Th>Nro.</Th>
+                        <Th>Nro. da Mãe</Th>
+                        <Th>Sexo</Th>
+                        <Th>Data de nascimento</Th>
+                        <Th>Marca</Th>
+                        <Th>Desmamado</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {animals && animals.map(({ id, numero, sexo, nascimento, marca, desmama }: Animal) => {
+                        return (
+                            <Tr key={id.toString()}>
+                                <Td isNumeric>{id}</Td>
+                                <Td>{numero}</Td>
+                                <Td>{sexo}</Td>
+                                <Td>{nascimento}</Td>
+                                <Td>{marca}</Td>
+                                <Td>{desmama ? "V" : "F"}</Td>
+                            </Tr>
+                        )
+                    })}
+                </Tbody>
+            </Table>
+        </TableContainer>
+    )
 }
 
 export default Home
